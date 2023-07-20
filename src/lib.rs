8e78ce5857726c80821d8d95c90f595fe5206f0b
HimbeerserverDE <himbeerserverde@gmail.com>
2023-07-20 18:55:00 +0200

    use string literals instead of identifiers for attribute map accesses

--- src/lib.rs
+++ src/lib.rs
@@ -146,7 +146,7 @@ pub fn derive_deserialize(item: TokenStream) -> TokenStream {
         if len_for.contains_key(&field_name.to_string()) {
             out.extend(
                 vec![quote!(
-                    let r = r.take(*len_for.get(String::from(#field_name)).unwrap() as u64);
+                    let r = r.take(*len_for.get("#field_name").unwrap() as u64);
                 )]
                 .into_iter(),
             );
@@ -155,7 +155,7 @@ pub fn derive_deserialize(item: TokenStream) -> TokenStream {
         if discriminant_for.contains_key(&field_name.to_string()) {
             out.extend(
                 vec![quote!(
-                    let attr = discriminant_for.get(String::from(#field_name)).unwrap();
+                    let attr = discriminant_for.get("#field_name").unwrap();
                     self.#field_name.deserialize_with_discriminant(r, attr)?;
                 )]
                 .into_iter(),
